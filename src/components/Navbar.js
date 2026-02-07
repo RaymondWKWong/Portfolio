9cb7eb035cbb60fe7275735b8672e4c974656dd2
Raymond Wong <90529699+RaymondWKWong@users.noreply.github.com>
2026-02-07 17:50:31 +0000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -102,17 +102,6 @@ function NavBar() {
                 <ImBlog style={{ marginBottom: "2px" }} /> Blogs
               </Nav.Link>
             </Nav.Item> */}
-
-            <Nav.Item className="fork-btn">
-              <Button
-                href="https://github.com/RaymondWKWong"
-                target="_blank"
-                className="fork-btn-inner"
-              >
-                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
-                <AiFillStar style={{ fontSize: "1.1em" }} />
-              </Button>
-            </Nav.Item>
           </Nav>
         </Navbar.Collapse>
       </Container>
